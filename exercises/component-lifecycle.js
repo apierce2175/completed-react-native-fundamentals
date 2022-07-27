6d54407c640f4c491865c0fc28a0862388359bbc
apierce2175 <apierce@thecargoagency.com>
2022-07-27 16:58:03 -0400

    finished component lifecycle

--- exercises/component-lifecycle.js
+++ exercises/component-lifecycle.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
-
-import { listener, listener2 } from "../support/component-lifecycle";
 
 export default () => {
   const [updateCount, setUpdateCount] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdateCount(seconds => seconds + 1);
+      console.log(updateCount);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+      console.log("unsubscribed");
+    }
+  }, );
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -12,3 +20,27 @@ export default () => {
     </View>
   );
 };
+
+
+
+
+// BELOW EXAMPLE UPDATES ONCE AFTER 3 SECONDS POST LOAD
+
+// import React, { useState, useEffect } from "react";
+// import { View, Text } from "react-native";
+
+// export default () => {
+//   const [updateCount, setUpdateCount] = useState(0);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setUpdateCount('3 seconds');
+//     }, 3000);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>The listener has updated {updateCount} time(s).</Text>
+//     </View>
+//   );
+// };
