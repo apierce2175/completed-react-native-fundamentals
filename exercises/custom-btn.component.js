4ee486bb93caf413a760f5435dc42549a5c371d9
apierce2175 <apierce@thecargoagency.com>
2022-07-27 17:03:04 -0400

    created a simple custom button component to import and use in the mulitple files exercise

---
+++ exercises/custom-btn.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+export const CustomButton = ({ text, onPress, buttonStyle = {} }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "#ff637c",
+      alignSelf: "center",
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      marginVertical: 15,
+      ...buttonStyle,
+    }}
+  >
+    <Text style={{ color: "#fff" }}>{text}</Text>
+  </TouchableOpacity>
+);
+
