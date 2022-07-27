4a3c78d4801bb5201abb16206329487c39ccf05c
apierce2175 <apierce@thecargoagency.com>
2022-07-27 16:58:30 -0400

    completed custom component

--- exercises/custom-component.js
+++ exercises/custom-component.js
@@ -1,6 +1,44 @@
-// Creating a Custom Component
+import React from "react";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 
-export default () => {
-  // return <Button text="Hello" onPress={() => alert('hello!')} />
-  return null;
-};
+const CustomButton = ({ onPress, text, customButtonStyle }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={customButtonStyle}
+  >
+    <Text>{text}</Text>
+  </TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+  customButtonPrimary: {
+    backgroundColor: '#C38D9E',
+    color: '#fff',
+    padding: 10,
+    margin: 10,
+    borderRadius: 4,
+  },
+  customButtonSecondary: {
+    backgroundColor: '#E27D60',
+    color: '#fff',
+    padding: 10,
+    margin: 10,
+    borderRadius: 4,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#85DCBC',
+  },
+});
+
+export default () => (
+  <View
+    style={styles.container}
+  >
+    <CustomButton text="This is the primary btn" customButtonStyle={styles.customButtonPrimary} onPress={() => alert("Pressed btn 1")} />
+    <CustomButton text="secondary" customButtonStyle={styles.customButtonSecondary} onPress={() => alert("Pressed btn 2")} />
+  </View>
+);
