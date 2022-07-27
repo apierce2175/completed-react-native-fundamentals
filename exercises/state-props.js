e4f684de825ffcc286865f2761298973ffde9c7f
apierce2175 <apierce@thecargoagency.com>
2022-07-27 17:01:01 -0400

    completed exercise on state props

--- exercises/state-props.js
+++ exercises/state-props.js
@@ -1,14 +1,20 @@
 // Using State and Props
 
 // useState is a hook that is a named export from React.
-import React from "react";
-import { Text, Button, View } from "react-native";
+import React, { useState } from "react";
+import { Text, Button, View, Dimensions } from "react-native";
+
+// Dimensions get full height and width of device screen so it can be used as 100vh or 100vw equivalent in styling
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export default () => {
+  const [count, setCount] = useState(0);
+  const btnTitle = "Press Me";
   return (
-    <View style={{ alignItems: "center", marginTop: 20 }}>
-      <Text>You've pressed the button: ??? time(s)</Text>
-      <Button title="Press me" onPress={() => {}} />
+    <View style={{ alignItems: "center", marginTop: 20, backgroundColor: '#FC4445', width: screenWidth, height: screenHeight }}>
+      <Text style={{color: "#3FEEE6"}}>You've pressed the button: {count} time(s)</Text>
+      <Button color="#CAFAFE" title={btnTitle} onPress={() => {setCount(count + 1)}} />
     </View>
   );
 };
