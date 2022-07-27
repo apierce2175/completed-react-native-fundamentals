978b2b12e8eedda4992083473904f6977fd542a6
apierce2175 <apierce@thecargoagency.com>
2022-07-27 16:57:34 -0400

    finished capturing taps

--- exercises/capturing-taps.js
+++ exercises/capturing-taps.js
@@ -1,9 +1,44 @@
-// Capturing User Taps
+import React from 'react';
+import { View, Button, Alert, TouchableOpacity, StyleSheet, Text } from 'react-native';
 
-// TIP: Button is a component exposed by React Native. You'll import it the same way you did View
-// and Text previously.
 
-export default () => {
-  // Replace return null with your code.
-  return null;
-};
+const CapturingTaps = () => (
+  <View style={styles.container}>
+    <Button
+      onPress={() => Alert.alert("User pressed btn 1")}
+      title="Button 1"
+      accessibilityLabel="Learn about btn 1"
+      color='#99738E'
+    />
+    <Button
+      onPress={() => Alert.alert("User pressed btn 2")}
+      title="Button 2"
+      accessibilityLabel="Learn about btn 2"
+      color='#553D67'
+    />
+    <TouchableOpacity
+      onPress={() => Alert.alert("User pressed touchable opacity")}
+      style={styles.buttonCustom}
+    >
+      <Text>Press Here</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    flexDirection: "column",
+    justifyContent: 'flex-start',
+    alignItems: "center",
+    backgroundColor: "#242582",
+  },
+  buttonCustom: {
+    backgroundColor: "#553D67",
+    borderRadius: 10,
+    padding: 10,
+  },
+});
+
+export default CapturingTaps;
